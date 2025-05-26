2dc6f7c57fc0da1565a1672d7755b540f41ffc67
elemermelada <iiasanmi9@gmail.com>
2025-05-26 22:49:46 +0200

    feat: first implementation

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const passwordInput = document.getElementById("passwordInput");
-const password = passwordInput.value;
-passwordInput.addEventListener("input", function (e) {
-  const password = e.target.value;
-  console.log("Password entered: " + password);
+let passwordState = passwordInput.value;
+
+passwordInput.addEventListener("keypress", function (e) {
+  const character = e.key;
+  passwordState += character; // Append the character to the password
+  passwordInput.value = censorPassword(passwordState); // Update the input value with censored password
+
+  e.preventDefault(); // Prevent the default action of the keypress
 });
+
+passwordInput.addEventListener("keydown", function (e) {
+  if (e.key === "Backspace" || e.key === "Delete") {
+    // TODO - handle backspace and delete using selectionStart and selectionEnd
+    // For now, just remove the last character from password
+    if (passwordState.length === 0) {
+      return; // Do nothing if password is empty
+    }
+    passwordState = passwordState.slice(0, -1);
+    passwordInput.value = censorPassword(passwordState); // Update the input value with censored password
+    e.preventDefault(); // Prevent the default action of the keydown
+  }
+});
+
+setInterval(() => {
+  console.log("Interval", passwordState);
+}, 1000);
+
+function censorPassword(password) {
+  // Replace every character with *
+  return Array(password.length).fill("*").join("");
+}
